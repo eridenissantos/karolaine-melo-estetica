7d37a057df511e81a9f0a61fdae5d5b7302c9af2
eridenissantos <eridenissantos@gmail.com>
2026-01-25 01:31:05 -0300

    concluindo a pagina principal

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById("btnFlutuante");
+
+window.addEventListener("scroll", () => {
+    if (window.scrollY <= 300) {
+        btn.style.display = "none";
+    } else {
+        btn.style.display = "flex";
+    }
+});
